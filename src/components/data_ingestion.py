e2afae33fac1cf685877ca16cffc11e9cdc4b99e
Daya2k <dayanandhjangam@gmail.com>
2024-07-09 01:40:02 -0400

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,54 @@
 import os
 import sys
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from src.exception import CustomException
 from src.logger import logging
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:
+    '''
+    To save data in the specified path
+    '''
+    train_data_path: str = os.path.join('artifacts', 'train.pkl')
+    test_data_path: str = os.path.join('artifacts', 'test.pkl')
+    raw_data_path: str = os.path.join('artifacts', 'data.parquet')
+
+
+class DataIngestion:
+    '''Acquiring data from sources'''
+
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        '''Data Ingestion'''
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df = pd.read_parquet('notebooks/data/merged_data.parquet')
+            logging.info('read the parquet dataset as dataframe')
+
+            os.makedirs(os.path.dirname(
+                self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_parquet(self.ingestion_config.raw_data_path)
+
+            logging.info("Train Test Split Started")
+            train_set, test_set = train_test_split(
+                df, test_size=0.2, random_state=42, stratify=df.classes)
+
+            train_set.to_pickle(self.ingestion_config.train_data_path)
+            test_set.to_pickle(self.ingestion_config.test_data_path)
+
+            logging.info("Ingestion of the data is completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
